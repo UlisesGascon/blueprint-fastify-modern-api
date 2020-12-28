4c52d8e921c4201f1ae0dd4566de0e83ac3c4258
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-12-28 12:54:02 +0100

    feat: added basic http server
    - Added depencency `fastify@^3.9.2`
    - Added sample env file
    - Added `/__/health` route and  server initialization

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello world!')
+require('dotenv').config()
+const fastify = require('fastify')({ logger: true })
+
+const port = process.env.PORT || 3000
+
+fastify.get('/__/health', async (request, reply) => {
+  return { status: 'ok' }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(port)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
